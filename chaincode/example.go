0e30fb698fa2d3cbec45023df2fafa1022e45987
Till Lindemark <till.lindemark@gmail.com>
2017-08-11 12:10:09 +0200

    Better logging and use of error codes in the chaincode

--- chaincode/example.go
+++ chaincode/example.go
@@ -1,26 +1,4 @@
-/*
-Copyright IBM Corp. 2016 All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-		 http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package main
-
-//WARNING - this chaincode's ID is hard-coded in chaincode_example04 to illustrate one way of
-//calling chaincode from a chaincode. If this example is modified, chaincode_example04.go has
-//to be modified as well with the new ID of chaincode_example02.
-//chaincode_example05 show's how chaincode ID can be passed in as a parameter instead of
-//hard-coding.
 
 import (
 	"fmt"
@@ -34,81 +12,106 @@ import (
 // SimpleChaincode example simple Chaincode implementation
 type SimpleChaincode struct{}
 
+//Initialization step
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
 	log.Printf("--> INITIALIZING")
-	return shim.Success([]byte("All is ok"))
+	return shim.Success(nil)
 }
 
+//Query gets the value of the index given
+// global so returns a success or error code with the payload containing the error or the value of the index
 func Query(stub shim.ChaincodeStubInterface, index string) pb.Response {
+	log.Printf("--> QUERY")
 	if a, err := stub.GetState(index); err != nil {
-		return shim.Error(err.Error())
+		return shim.Error("0_0_0 " + err.Error())
 	} else {
-		log.Printf("a : %s", a)
+		log.Printf("    %s : %s", index, a)
+		log.Printf("    END Query")
 		return shim.Success([]byte(a))
 	}
 }
 
+// Set creates or update the index with the value given
+// global so returns a success or error code with the payload containing the error or the index and new value
 func Set(stub shim.ChaincodeStubInterface, index string, value int) pb.Response {
+	log.Printf("--> SET")
 	if err := stub.PutState(index, []byte(strconv.Itoa(value))); err != nil {
-		return shim.Error(err.Error())
+		return shim.Error("0_1_0 " + err.Error())
 	}
 	response := index + ": " + strconv.Itoa(value)
+	log.Printf("    %s", response)
+	log.Printf("    END SET")
 	return shim.Success([]byte(response))
 }
 
+//add adds the value to the index
+// returns the new value or the error created
 func add(stub shim.ChaincodeStubInterface, index string, value int) (int, error) {
+	log.Printf("-----> Add")
 	a, err := stub.GetState(index)
 	if err != nil {
-		return 0, fmt.Errorf("Couldn't access the state of %s : %s", index, err)
+		return 0, fmt.Errorf("0_2_0 Couldn't access the state of %s : %s", index, err)
 	}
-	log.Printf("Value of %s before modification : %s", index, a)
+	log.Printf("       Value of %s before modification : %s", index, a)
 	aInt, err := strconv.Atoi(string(a))
 	if err != nil {
-		return 0, fmt.Errorf("Couldn't set the value to int : %s", err)
+		return 0, fmt.Errorf("0_2_1 Couldn't set the value to int : %s", err)
 	}
 	newA := value + aInt
 	if err := stub.PutState("a", []byte(strconv.Itoa(newA))); err != nil {
-		return 0, fmt.Errorf("Couldn't put the value into the new state : %s", err)
+		return 0, fmt.Errorf("0_2_2 Couldn't put the value into the new state : %s", err)
 	}
-	log.Printf("Value of the index after modification : %d", newA)
+	log.Printf("       Value of the index after modification : %d", newA)
+	log.Printf("       END Add")
 	return newA, nil
 }
 
+//substract substracts the value to the index
+// returns the value or the error created
 func substract(stub shim.ChaincodeStubInterface, index string, value int) (int, error) {
+	log.Printf("-----> Substract")
 	a, err := stub.GetState(index)
 	if err != nil {
-		return 0, fmt.Errorf("Couldn't access the state of %s : %s", index, err)
+		return 0, fmt.Errorf("0_3_0 Couldn't access the state of %s : %s", index, err)
 	}
-	log.Printf("Value of the index before modification : %s", a)
+	log.Printf("       Value of the index %s before modification : %s", index, a)
 	aInt, err := strconv.Atoi(string(a))
 	if err != nil {
-		return 0, fmt.Errorf("Couldn't transform the value to int : %s", err)
+		return 0, fmt.Errorf("0_3_1 Couldn't transform the value to int : %s", err)
 	}
 	newA := aInt - value
 	if newA > 0 {
 		if err := stub.PutState(index, []byte(strconv.Itoa(newA))); err != nil {
-			return 0, fmt.Errorf("Couldn't write the new value in the chaincode : s%s", err)
+			return 0, fmt.Errorf("0_3_2 Couldn't write the new value in the chaincode : s%s", err)
 		}
-		log.Printf("Value of the index after modification : %d", newA)
+		log.Printf("        Value of the index after modification : %d", newA)
+		log.Printf("        END Substract")
 		return newA, nil
 	}
-	return 0, fmt.Errorf("Couldn't substract as not enough token")
+	return 0, fmt.Errorf("0/3/4 Couldn't substract as not enough token")
 }
 
+// Transaction transfers the value from index1 to index2
+// global so returns a success or error code with a payload containing the error or the new values of the indexes
 func Transaction(stub shim.ChaincodeStubInterface, index1 string, index2 string, value int) pb.Response {
+	log.Printf("--> TRANSACTION")
 	val1, err := substract(stub, index1, value)
 	if err != nil {
-		return shim.Error(err.Error())
+		return shim.Error("0_4_0 " + err.Error())
 	}
 
 	val2, err := add(stub, index2, value)
 	if err != nil {
-		return shim.Error(err.Error())
+		return shim.Error("0_4_1 " + err.Error())
 	}
 	response := index1 + ": " + strconv.Itoa(val1) + " and " + index2 + ": " + strconv.Itoa(val2)
+	fmt.Printf("    %s", response)
+	fmt.Printf("    END TRANSACTION")
 	return shim.Success([]byte(response))
 }
 
+// Invoke is the gateway of the chaincode, verifying the arguments given and calling the right function
+// transmits the results of the global functions or an error of bad call
 func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 	function, args := stub.GetFunctionAndParameters()
 	switch function {
@@ -119,7 +122,7 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 		index := args[0]
 		value, err := strconv.Atoi(args[1])
 		if err != nil {
-			return shim.Error(err.Error())
+			return shim.Error("0_I_0" + err.Error())
 		}
 		return Set(stub, index, value)
 	case "Transaction":
@@ -127,17 +130,18 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 		index2 := args[1]
 		value, err := strconv.Atoi(args[2])
 		if err != nil {
-			return shim.Error(err.Error())
+			return shim.Error("0_I_1 " + err.Error())
 		}
 		return Transaction(stub, index1, index2, value)
 	default:
-		return shim.Error("Wrong function in parameters : Add(s, int), Query(s), Substract(s,int) are available")
+		return shim.Error("0_I_2 Wrong function in parameters : Set(string, int) Transaction(string, string, int) are available")
 	}
 }
 
+// Simply launches the chaincode
 func main() {
 	err := shim.Start(new(SimpleChaincode))
 	if err != nil {
-		fmt.Printf("Error starting Simple chaincode: %s", err)
+		fmt.Printf("O_S_0 Error starting Simple chaincode: %s", err)
 	}
 }
