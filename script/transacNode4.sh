be5552245aa632bbdf975ef3dd9f448195453584
Till Lindemark <till.lindemark@gmail.com>
2017-08-17 11:54:31 +0200

    Part 2 of the benchmark

--- script/transacNode4.sh
+++ script/transacNode4.sh
@@ -4,5 +4,5 @@ while true
 do
 	peer chaincode invoke -o orderer.example.com:7050 --tls true --cafile /ca.crt  -C mychannel -n chaincode -v 1.0  -c '{"Args":["Transaction", "node3","node4", "10"]}'
 	peer chaincode invoke -o orderer.example.com:7050 --tls true --cafile /ca.crt  -C mychannel -n chaincode -v 1.0  -c '{"Args":["Transaction", "node6","node4", "10"]}'
-	sleep 7
+	sleep 1
 done
