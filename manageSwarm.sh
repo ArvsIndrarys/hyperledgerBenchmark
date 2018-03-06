bea3ba5a19e9e466677892f54960e0f2eb29433d
Marc Majewski <marc.majewski@talium.com>
2018-03-06 18:20:16 +0100

    translation and enhancements

--- manageSwarm.sh
+++ manageSwarm.sh
@@ -2,22 +2,16 @@
 
 COMMAND="$1"
 
-# On affiche ces indications si le script est mal appelé
+# print help if wrong option is used
 function printHelp {
     echo "Usage : ./manageSwarm < swarm-up|up|start|stop|down|swarm-down > if up, creates certificates and gives it to the other swarm members, then starts the service. If down, suppresses all that."
     echo "start exports only the containers on each peer, stops removes them and cleans without stopping the swarm or removing the certificates"
     echo "swarm-up and swarm-down respectively initiate the swarm or removes it"
 }
 
-# On initialize le swarm en propageant la commande contenant un token auto-généré puis en lançant cette commande
+# swarm propagation
 function swarm-up {
-
-    echo
-    echo
     echo "----- Creating swarm and duplicating keys ------"
-    echo
-    echo
-
     docker swarm init > ./joinCommand
    sed -i '1,4d' ./joinCommand && sed -i '4,7d' ./joinCommand
    chmod a+x ./joinCommand
@@ -36,20 +30,13 @@ function swarm-up {
 	rm ./joinCommand
 }
 
-# On crée les artifacts - certificats (blockchain privée) et donnée d'initialisation de la blockchain - à chaque noeud s'il est up
-# Puis on lance le script docker qui lance tout les containers (peers) puis la blockchain en elle même
+# artifacts creation ( certs and blockchain's init data ) and propagation to all nodes if they are up
+# then call to the script that creates all blockchain components
 function up {
-    
-    echo
-    echo
     echo "================================================"
     echo "============= Initializing nodes ==============="
     echo "================================================"
-    echo
-    echo
     echo "------------- Generating artifacts -------------"
-    echo
-    echo
 
     ./generateArtifacts.sh
    for i in {node0,node1,node2,node3,node4,node6}; do
@@ -63,15 +50,10 @@ function up {
    fi
    done
 
-    echo "------------- Deploying hyperledger ------------"
-    docker network create --driver=overlay --attachable hyperledgerBenchmark_default
-    docker stack deploy -c docker-compose.yaml hyperledgerBenchmark 	
-    sleep 1
-    docker exec -it $(docker ps -f name=hyperledgerBenchmark_cli --format "{{ .Names }}") './script/script.sh'
+   start
 }
 
-# Dans le cas où on a stoppé la blockchain sans supprimer les données de départ, on ne fait que relancer Docker
-# Utile pour les modifications d'appoint et pour éviter des dialogues réseau inutiles
+# if we didn't fully removed the blockchain (certs, channel artifacts and data) launch it back
 function start {
     echo "------------- Deploying hyperledger ------------"
     docker network create --driver=overlay --attachable hyperledgerBenchmark_default
@@ -80,12 +62,14 @@ function start {
     docker exec -it $(docker ps -f name=hyperledgerBenchmark_cli --format "{{ .Names }}") './script/script.sh' 
 }
 
-#On arrête la blockchain sans supprimer les données initiales ou les certificats
-#Utile pour les modifications d'appoint
+# just stopping the blockchain
 function stop {
     echo "------------ Stopping hyperledger --------------"
-    docker service rm hyperledgerBenchmark_cli hyperledgerBenchmark_peer0org1 hyperledgerBenchmark_peer1org1 hyperledgerBenchmark_peer2org1 hyperledgerBenchmark_peer3org1 hyperledgerBenchmark_peer4org1 hyperledgerBenchmark_peer5org1 hyperledgerBenchmark_peer6org1 hyperledgerBenchmark_orderer 
-	docker stop $(docker ps -a -f name='dev*' -q); docker rm $(docker ps -a -f name='dev*' -q); docker rmi $(docker images -f reference='*chaincode*' -q)
+    docker stack rm  hyperledgerBenchmark
+	docker stop $(docker ps -a -f name='dev*' -q)
+	docker rm $(docker ps -a -f name='dev*' -q)
+	docker rmi $(docker images -f reference='*chaincode*' -q)
+	
 	for i in {node0,node1,node2,node3,node4,node6};do 
 	ssh -q "$i" exit
 	if [ "$?" = '0' ];then
@@ -99,41 +83,28 @@ function stop {
     docker volume prune -f
 }
 
-#On supprime tout ce qui a été fait au niveau de la blockchain. Relancer le script avec un up aura changé toutes les données auto-générées (données initiales et certificats)
+# full blockchain removal (with certs and channel-artifacts)
 function terminate {
     echo "================================================"
     echo "============ Terminating nodes ================="
     echo "================================================"
-    echo
-    echo
     echo "-------- Stopping hyperledger services ---------"
-    echo
-    echo
 
-    docker service rm hyperledgerBenchmark_cli hyperledgerBenchmark_peer0org1 hyperledgerBenchmark_peer1org1 hyperledgerBenchmark_peer2org1 hyperledgerBenchmark_peer3org1 hyperledgerBenchmark_peer4org1 hyperledgerBenchmark_peer5org1 hyperledgerBenchmark_peer6org1 hyperledgerBenchmark_orderer 
-	docker stop $(docker ps -a -f name='dev*' -q); docker rm $(docker ps -a -f name='dev*' -q); docker rmi $(docker images -f reference='*chaincode*' -q)
-    rm -r crypto-config/ channel-artifacts/
-    docker network prune -f
-    docker volume prune -f
+   stop
    for i in {node0,node1,node2,node3,node4,node6};do 
    ssh -q "$i" exit
    if [ "$?" = '0' ];then
        echo "$i is up"
        ssh "$i" 'rm -r hyperledgerBenchmark/crypto-config hyperledgerBenchmark/channel-artifacts'
-   	ssh "$i" 'docker stop $(docker ps -a -f name='dev*' -q); docker rm $(docker ps -a -f name='dev*' -q); docker rmi $(docker images -f reference='*chaincode*' -q)'
    else
        echo "$? - $i is down"
    fi
    done
 }
 
-#On retire chacun des noeuds du swarm avant de le supprimer
+# swarm destruction
 function swarm-down {
-
-    echo
     echo "--------------- Removing swarm ----------------"
-    echo
-    echo
 
    for i in {node0,node1,node2,node3,node4,node6}; do
    ssh -q "$i" exit
